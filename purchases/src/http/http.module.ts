b9dec78aa999096233b74819b8080fb8addaa2be
Andrew Matheus <andrewmatheus@gmail.com>
2022-04-04 21:41:59 -0300

    IgniteLab-Aula1 - Estrutura dos projetos

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+
+import { TestResolver } from './test.resolver';
+import { DatabaseModule } from '../database/database.module';
+
+import path from 'node:path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [TestResolver],
+})
+export class HttpModule {}
